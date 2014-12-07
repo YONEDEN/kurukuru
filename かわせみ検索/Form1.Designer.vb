8b18482971c2c8510727c968fe8fc929ebffe44a
JYL <jyl2142@gmail.com>
2014-12-07 09:41:58 +0900

    フォームほぼまとまった。

--- かわせみ検索/Form1.Designer.vb
+++ かわせみ検索/Form1.Designer.vb
@@ -34,7 +34,6 @@ Partial Class Form1
         Me.TextBox7 = New System.Windows.Forms.TextBox()
         Me.TextBox8 = New System.Windows.Forms.TextBox()
         Me.TextBox9 = New System.Windows.Forms.TextBox()
-        Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
@@ -69,7 +68,6 @@ Partial Class Form1
         Me.TextBox34 = New System.Windows.Forms.TextBox()
         Me.TextBox35 = New System.Windows.Forms.TextBox()
         Me.TextBox36 = New System.Windows.Forms.TextBox()
-        Me.Label10 = New System.Windows.Forms.Label()
         Me.TextBox30 = New System.Windows.Forms.TextBox()
         Me.TextBox31 = New System.Windows.Forms.TextBox()
         Me.TextBox37 = New System.Windows.Forms.TextBox()
@@ -84,6 +82,12 @@ Partial Class Form1
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Label11 = New System.Windows.Forms.Label()
         Me.TextBox44 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -186,19 +190,10 @@ Partial Class Form1
         Me.TextBox9.TabIndex = 11
         Me.TextBox9.Text = "連絡先FAX"
         '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(516, 227)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(53, 12)
-        Me.Label4.TabIndex = 12
-        Me.Label4.Text = "情報公開"
-        '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(521, 257)
+        Me.Label5.Location = New System.Drawing.Point(12, 24)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(53, 12)
         Me.Label5.TabIndex = 13
@@ -207,7 +202,7 @@ Partial Class Form1
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(521, 278)
+        Me.Label6.Location = New System.Drawing.Point(12, 45)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(65, 12)
         Me.Label6.TabIndex = 14
@@ -216,7 +211,7 @@ Partial Class Form1
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(521, 302)
+        Me.Label7.Location = New System.Drawing.Point(12, 69)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(53, 12)
         Me.Label7.TabIndex = 15
@@ -225,7 +220,7 @@ Partial Class Form1
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(521, 327)
+        Me.Label8.Location = New System.Drawing.Point(12, 94)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(100, 12)
         Me.Label8.TabIndex = 16
@@ -345,7 +340,7 @@ Partial Class Form1
         'RadioButton1
         '
         Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(633, 254)
+        Me.RadioButton1.Location = New System.Drawing.Point(124, 21)
         Me.RadioButton1.Name = "RadioButton1"
         Me.RadioButton1.Size = New System.Drawing.Size(47, 16)
         Me.RadioButton1.TabIndex = 30
@@ -356,7 +351,7 @@ Partial Class Form1
         'RadioButton2
         '
         Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(686, 254)
+        Me.RadioButton2.Location = New System.Drawing.Point(177, 21)
         Me.RadioButton2.Name = "RadioButton2"
         Me.RadioButton2.Size = New System.Drawing.Size(114, 16)
         Me.RadioButton2.TabIndex = 31
@@ -367,7 +362,7 @@ Partial Class Form1
         'RadioButton3
         '
         Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(686, 279)
+        Me.RadioButton3.Location = New System.Drawing.Point(177, 46)
         Me.RadioButton3.Name = "RadioButton3"
         Me.RadioButton3.Size = New System.Drawing.Size(114, 16)
         Me.RadioButton3.TabIndex = 33
@@ -378,7 +373,7 @@ Partial Class Form1
         'RadioButton4
         '
         Me.RadioButton4.AutoSize = True
-        Me.RadioButton4.Location = New System.Drawing.Point(633, 279)
+        Me.RadioButton4.Location = New System.Drawing.Point(124, 46)
         Me.RadioButton4.Name = "RadioButton4"
         Me.RadioButton4.Size = New System.Drawing.Size(47, 16)
         Me.RadioButton4.TabIndex = 32
@@ -388,7 +383,7 @@ Partial Class Form1
         '
         'TextBox22
         '
-        Me.TextBox22.Location = New System.Drawing.Point(633, 299)
+        Me.TextBox22.Location = New System.Drawing.Point(124, 66)
         Me.TextBox22.Name = "TextBox22"
         Me.TextBox22.Size = New System.Drawing.Size(212, 19)
         Me.TextBox22.TabIndex = 34
@@ -396,7 +391,7 @@ Partial Class Form1
         '
         'TextBox23
         '
-        Me.TextBox23.Location = New System.Drawing.Point(633, 324)
+        Me.TextBox23.Location = New System.Drawing.Point(124, 91)
         Me.TextBox23.Name = "TextBox23"
         Me.TextBox23.Size = New System.Drawing.Size(212, 19)
         Me.TextBox23.TabIndex = 35
@@ -404,7 +399,7 @@ Partial Class Form1
         '
         'TextBox24
         '
-        Me.TextBox24.Location = New System.Drawing.Point(521, 349)
+        Me.TextBox24.Location = New System.Drawing.Point(12, 116)
         Me.TextBox24.Name = "TextBox24"
         Me.TextBox24.Size = New System.Drawing.Size(159, 19)
         Me.TextBox24.TabIndex = 36
@@ -412,7 +407,7 @@ Partial Class Form1
         '
         'TextBox25
         '
-        Me.TextBox25.Location = New System.Drawing.Point(687, 349)
+        Me.TextBox25.Location = New System.Drawing.Point(178, 116)
         Me.TextBox25.Name = "TextBox25"
         Me.TextBox25.Size = New System.Drawing.Size(159, 19)
         Me.TextBox25.TabIndex = 37
@@ -420,7 +415,7 @@ Partial Class Form1
         '
         'TextBox26
         '
-        Me.TextBox26.Location = New System.Drawing.Point(521, 374)
+        Me.TextBox26.Location = New System.Drawing.Point(12, 141)
         Me.TextBox26.Name = "TextBox26"
         Me.TextBox26.Size = New System.Drawing.Size(159, 19)
         Me.TextBox26.TabIndex = 38
@@ -428,7 +423,7 @@ Partial Class Form1
         '
         'TextBox27
         '
-        Me.TextBox27.Location = New System.Drawing.Point(686, 374)
+        Me.TextBox27.Location = New System.Drawing.Point(177, 141)
         Me.TextBox27.Name = "TextBox27"
         Me.TextBox27.Size = New System.Drawing.Size(159, 19)
         Me.TextBox27.TabIndex = 39
@@ -436,14 +431,14 @@ Partial Class Form1
         '
         'TextBox28
         '
-        Me.TextBox28.Location = New System.Drawing.Point(677, 195)
+        Me.TextBox28.Location = New System.Drawing.Point(168, 99)
         Me.TextBox28.Name = "TextBox28"
         Me.TextBox28.Size = New System.Drawing.Size(153, 19)
         Me.TextBox28.TabIndex = 46
         '
         'TextBox29
         '
-        Me.TextBox29.Location = New System.Drawing.Point(518, 170)
+        Me.TextBox29.Location = New System.Drawing.Point(9, 74)
         Me.TextBox29.Name = "TextBox29"
         Me.TextBox29.Size = New System.Drawing.Size(90, 19)
         Me.TextBox29.TabIndex = 45
@@ -451,7 +446,7 @@ Partial Class Form1
         '
         'TextBox32
         '
-        Me.TextBox32.Location = New System.Drawing.Point(518, 195)
+        Me.TextBox32.Location = New System.Drawing.Point(9, 99)
         Me.TextBox32.Name = "TextBox32"
         Me.TextBox32.Size = New System.Drawing.Size(153, 19)
         Me.TextBox32.TabIndex = 42
@@ -459,15 +454,15 @@ Partial Class Form1
         '
         'TextBox33
         '
-        Me.TextBox33.Location = New System.Drawing.Point(614, 170)
+        Me.TextBox33.Location = New System.Drawing.Point(105, 74)
         Me.TextBox33.Name = "TextBox33"
-        Me.TextBox33.Size = New System.Drawing.Size(249, 19)
+        Me.TextBox33.Size = New System.Drawing.Size(244, 19)
         Me.TextBox33.TabIndex = 41
         Me.TextBox33.Text = "代表者住所"
         '
         'TextBox34
         '
-        Me.TextBox34.Location = New System.Drawing.Point(518, 145)
+        Me.TextBox34.Location = New System.Drawing.Point(9, 49)
         Me.TextBox34.Name = "TextBox34"
         Me.TextBox34.Size = New System.Drawing.Size(249, 19)
         Me.TextBox34.TabIndex = 40
@@ -483,24 +478,15 @@ Partial Class Form1
         '
         'TextBox36
         '
-        Me.TextBox36.Location = New System.Drawing.Point(518, 123)
+        Me.TextBox36.Location = New System.Drawing.Point(9, 27)
         Me.TextBox36.Name = "TextBox36"
         Me.TextBox36.Size = New System.Drawing.Size(249, 19)
         Me.TextBox36.TabIndex = 48
         Me.TextBox36.Text = "代表者ふりがな"
         '
-        'Label10
-        '
-        Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(521, 409)
-        Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(81, 12)
-        Me.Label10.TabIndex = 49
-        Me.Label10.Text = "事務処理データ"
-        '
         'TextBox30
         '
-        Me.TextBox30.Location = New System.Drawing.Point(521, 435)
+        Me.TextBox30.Location = New System.Drawing.Point(13, 14)
         Me.TextBox30.Name = "TextBox30"
         Me.TextBox30.Size = New System.Drawing.Size(159, 19)
         Me.TextBox30.TabIndex = 50
@@ -508,7 +494,7 @@ Partial Class Form1
         '
         'TextBox31
         '
-        Me.TextBox31.Location = New System.Drawing.Point(521, 460)
+        Me.TextBox31.Location = New System.Drawing.Point(13, 39)
         Me.TextBox31.Name = "TextBox31"
         Me.TextBox31.Size = New System.Drawing.Size(159, 19)
         Me.TextBox31.TabIndex = 51
@@ -516,7 +502,7 @@ Partial Class Form1
         '
         'TextBox37
         '
-        Me.TextBox37.Location = New System.Drawing.Point(521, 485)
+        Me.TextBox37.Location = New System.Drawing.Point(13, 64)
         Me.TextBox37.Name = "TextBox37"
         Me.TextBox37.Size = New System.Drawing.Size(159, 19)
         Me.TextBox37.TabIndex = 52
@@ -548,7 +534,7 @@ Partial Class Form1
         '
         'TextBox41
         '
-        Me.TextBox41.Location = New System.Drawing.Point(521, 510)
+        Me.TextBox41.Location = New System.Drawing.Point(13, 89)
         Me.TextBox41.Name = "TextBox41"
         Me.TextBox41.Size = New System.Drawing.Size(159, 19)
         Me.TextBox41.TabIndex = 56
@@ -556,7 +542,7 @@ Partial Class Form1
         '
         'TextBox42
         '
-        Me.TextBox42.Location = New System.Drawing.Point(688, 510)
+        Me.TextBox42.Location = New System.Drawing.Point(180, 89)
         Me.TextBox42.Name = "TextBox42"
         Me.TextBox42.Size = New System.Drawing.Size(159, 19)
         Me.TextBox42.TabIndex = 57
@@ -573,7 +559,7 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(746, 9)
+        Me.Button2.Location = New System.Drawing.Point(744, 13)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(119, 23)
         Me.Button2.TabIndex = 59
@@ -616,43 +602,76 @@ Partial Class Form1
         Me.TextBox44.TabIndex = 63
         Me.TextBox44.Text = "メモ"
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox27)
+        Me.GroupBox1.Controls.Add(Me.TextBox26)
+        Me.GroupBox1.Controls.Add(Me.TextBox25)
+        Me.GroupBox1.Controls.Add(Me.TextBox24)
+        Me.GroupBox1.Controls.Add(Me.TextBox23)
+        Me.GroupBox1.Controls.Add(Me.TextBox22)
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.RadioButton4)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Location = New System.Drawing.Point(507, 237)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(353, 176)
+        Me.GroupBox1.TabIndex = 64
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "情報公開"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox42)
+        Me.GroupBox2.Controls.Add(Me.TextBox41)
+        Me.GroupBox2.Controls.Add(Me.TextBox37)
+        Me.GroupBox2.Controls.Add(Me.TextBox31)
+        Me.GroupBox2.Controls.Add(Me.TextBox30)
+        Me.GroupBox2.Location = New System.Drawing.Point(507, 419)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(351, 111)
+        Me.GroupBox2.TabIndex = 65
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "事務処理データ"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.TextBox36)
+        Me.GroupBox3.Controls.Add(Me.TextBox28)
+        Me.GroupBox3.Controls.Add(Me.TextBox29)
+        Me.GroupBox3.Controls.Add(Me.TextBox32)
+        Me.GroupBox3.Controls.Add(Me.TextBox33)
+        Me.GroupBox3.Controls.Add(Me.TextBox34)
+        Me.GroupBox3.Location = New System.Drawing.Point(507, 103)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(353, 118)
+        Me.GroupBox3.TabIndex = 66
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "代表者情報"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(872, 651)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.TextBox44)
         Me.Controls.Add(Me.Label11)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.TextBox43)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox42)
-        Me.Controls.Add(Me.TextBox41)
         Me.Controls.Add(Me.TextBox40)
         Me.Controls.Add(Me.TextBox39)
         Me.Controls.Add(Me.TextBox38)
-        Me.Controls.Add(Me.TextBox37)
-        Me.Controls.Add(Me.TextBox31)
-        Me.Controls.Add(Me.TextBox30)
-        Me.Controls.Add(Me.Label10)
-        Me.Controls.Add(Me.TextBox36)
         Me.Controls.Add(Me.TextBox35)
-        Me.Controls.Add(Me.TextBox28)
-        Me.Controls.Add(Me.TextBox29)
-        Me.Controls.Add(Me.TextBox32)
-        Me.Controls.Add(Me.TextBox33)
-        Me.Controls.Add(Me.TextBox34)
-        Me.Controls.Add(Me.TextBox27)
-        Me.Controls.Add(Me.TextBox26)
-        Me.Controls.Add(Me.TextBox25)
-        Me.Controls.Add(Me.TextBox24)
-        Me.Controls.Add(Me.TextBox23)
-        Me.Controls.Add(Me.TextBox22)
-        Me.Controls.Add(Me.RadioButton3)
-        Me.Controls.Add(Me.RadioButton4)
-        Me.Controls.Add(Me.RadioButton2)
-        Me.Controls.Add(Me.RadioButton1)
         Me.Controls.Add(Me.TextBox21)
         Me.Controls.Add(Me.TextBox20)
         Me.Controls.Add(Me.TextBox19)
@@ -666,11 +685,6 @@ Partial Class Form1
         Me.Controls.Add(Me.TextBox11)
         Me.Controls.Add(Me.TextBox10)
         Me.Controls.Add(Me.Label9)
-        Me.Controls.Add(Me.Label8)
-        Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.TextBox9)
         Me.Controls.Add(Me.TextBox8)
         Me.Controls.Add(Me.TextBox7)
@@ -685,6 +699,12 @@ Partial Class Form1
         Me.Controls.Add(Me.TextBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -701,7 +721,6 @@ Partial Class Form1
     Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
     Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
     Friend WithEvents TextBox9 As System.Windows.Forms.TextBox
-    Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents Label5 As System.Windows.Forms.Label
     Friend WithEvents Label6 As System.Windows.Forms.Label
     Friend WithEvents Label7 As System.Windows.Forms.Label
@@ -736,7 +755,6 @@ Partial Class Form1
     Friend WithEvents TextBox34 As System.Windows.Forms.TextBox
     Friend WithEvents TextBox35 As System.Windows.Forms.TextBox
     Friend WithEvents TextBox36 As System.Windows.Forms.TextBox
-    Friend WithEvents Label10 As System.Windows.Forms.Label
     Friend WithEvents TextBox30 As System.Windows.Forms.TextBox
     Friend WithEvents TextBox31 As System.Windows.Forms.TextBox
     Friend WithEvents TextBox37 As System.Windows.Forms.TextBox
@@ -751,5 +769,8 @@ Partial Class Form1
     Friend WithEvents Button3 As System.Windows.Forms.Button
     Friend WithEvents Label11 As System.Windows.Forms.Label
     Friend WithEvents TextBox44 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
 
 End Class
---
+++ かわせみ検索/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub TextBox8_TextChanged(sender As Object, e As EventArgs) Handles TextBox8.TextChanged
+
+    End Sub
+
+    Private Sub TextBox9_TextChanged(sender As Object, e As EventArgs) Handles TextBox9.TextChanged
+
+    End Sub
+
+    Private Sub TextBox6_TextChanged(sender As Object, e As EventArgs) Handles TextBox6.TextChanged
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox20_TextChanged(sender As Object, e As EventArgs) Handles TextBox20.TextChanged
+
+    End Sub
+
+    Private Sub TextBox43_TextChanged(sender As Object, e As EventArgs) Handles TextBox43.TextChanged
+
+    End Sub
+End Class
